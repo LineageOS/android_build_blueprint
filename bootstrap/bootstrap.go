ef75beb068a79a9ef6ed4130c441ed7ac032f8bc
Cole Faust <colefaust@google.com>
2024-01-02 16:58:49 -0800

    Add implicit inputs to primary builder actions
    This is needed so primary builder actions can directly depend on their
    glob result files.
    
    Bug: 318434287
    Test: rm -rf out && m nothing && m nothing
    Change-Id: I5c67ee53c9f18f81c79c0fe13b3338eacaccdbc0

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -760,6 +760,7 @@ func (s *singleton) GenerateBuildActions(ctx blueprint.SingletonContext) {
 			Rule:      generateBuildNinja,
 			Outputs:   i.Outputs,
 			Inputs:    i.Inputs,
+			Implicits: i.Implicits,
 			OrderOnly: i.OrderOnlyInputs,
 			Args: map[string]string{
 				"builder": primaryBuilderFile,
